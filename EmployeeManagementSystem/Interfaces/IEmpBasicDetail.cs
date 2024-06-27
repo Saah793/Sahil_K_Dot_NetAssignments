e2f6b04b1750f8dcbb91c4c5836f9ee016f2ff70
Sahil Khalkar <114798186+Saah793@users.noreply.github.com>
2024-06-27 13:16:01 +0530

    Add files via upload

---
+++ EmployeeManagementSystem/Interfaces/IEmpBasicDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Employee_Management_System.DTO;
+using Employee_Management_System.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee_Management_System.Interfaces
+{
+    public interface IEmpBasicDetail
+    {
+
+        Task<EmpBasicDTO> AddEmployee(EmpBasicDTO empBasicDTO);
+
+        Task<EmpBasicDTO> GetEmployeeByUId(string UId);
+       
+        Task<List<EmpBasicDTO>> GetAllEmployee();
+
+        Task<EmpBasicDTO> UpdateEmployee(EmpBasicDTO empBasicDTO);
+
+        Task<string> DeleteEmployee(string uId);
+
+        Task<List<EmpBasicDTO>> GetEmployeeByRole(string role);
+        Task<EmployeeFilter> GetEmployeebyServiceFilter(EmployeeFilter employeeFilter);
+        Task <EmpBasicDTO> GetEmployeeByRolenew(string role);
+        Task<List<EmpBasicDTO>> GetEmpBasicDetailByMakeRequest();
+        Task<IActionResult> AddEmpByMakePostRequest(EmpBasicDTO empBasicDto);
+
+        Task<List<EmpBasicDTO>> GetEmpByMakeGetRequest();
+    }
+}
